f4f66ce2a33e749d596308262b50cdbcf1bafb52
Abhi-Kushwah <abhishekkushwah75094@gmail.com>
2025-02-14 23:20:38 +0300

    calculator

---
+++ Calculator/src/add/Addition.java
@@ -0,0 +1,13 @@
+package add;
+
+import java.util.Scanner;
+
+public class Addition {
+	public static void main(String[] args) {
+		int x=10;
+		int y=23;
+		System.out.println("aad : "+(x+y)+"\n multiply of x,y :"+(x*y));
+	 
+	}
+
+}
---
+++ Calculator/src/division/Divison.java
@@ -0,0 +1,12 @@
+package division;
+
+public class Divison {
+public static void main(String[] args) {
+	int a=10;
+	if(a%2==0) {
+		System.out.println("even number : "+a);
+	}
+	else{System.out.println("odd number : "+a);
+	}
+}
+}
